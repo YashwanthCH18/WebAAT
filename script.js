7303e9393ea0ac6d48f133c965d0c0c674a1dd77
YashwanthCH18 <148922289+YashwanthCH18@users.noreply.github.com>
2025-02-24 05:57:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ themeToggle.addEventListener('click', () => {
   document.body.style.backgroundColor = darkMode ? '#333' : '#f7f4f8';
   document.body.style.color = darkMode ? '#fff' : '#333';
 });
+
+window.onscroll = function() {
+  // Get the navbar element
+  var navbar = document.querySelector('.navbar');
+  
+  // If the page is scrolled down by 50px or more, add the 'scrolled' class to the navbar
+  if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+  } else {
+      navbar.classList.remove('scrolled');
+  }
+}
